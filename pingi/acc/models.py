072a75dc7fee69442ce386c7e1b981b75d268018
Sina Mobarez <mubarriizz@gmail.com>
2025-05-06 15:23:10 +0330

    [feat] create custom user with mobile as pk

--- pingi/acc/models.py
+++ pingi/acc/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, mobile, password=None, **extra_fields):
+        if not mobile:
+            raise ValueError('Mobile number is required')
+        user = self.model(mobile=mobile, **extra_fields)
+        if password:
+            user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, mobile, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(mobile, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    mobile = models.CharField(max_length=15, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    now_endpoint_count = models.IntegerField(default=0)
+
+    USERNAME_FIELD = 'mobile'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.mobile
+
+
+class OTPLog(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    otp = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user.mobile} - {self.otp}"
